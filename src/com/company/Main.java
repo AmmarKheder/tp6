aa932d365aa15ee8f43c870201c3268df2078675
Ammar Kheder <ammar.kheder.auditeur@lecnam.net>
2021-12-08 11:50:57 +0100

    Ex1,2,3-complet avec commentaire

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,24 +8,26 @@ public class Main {
         Division c = new Division();
         double resultat = c.division1(5,2);
         System.out.println(resultat);
-
+    //Si b=0 le programme bug et s'arrete
     }
     public static void ex2(){
         Division div=new Division();
         double resultat=div.division2(10,0);
         System.out.println(resultat);
+    // si b=0 on renvoie 0 mais ça reste un résulatat faux
     }
 
     public static void ex3() {
         Division c = new Division();
 
-        try { 
+        try {
             double res = c.division3(10, 0);
             System.out.println(res);
         }catch (IOException e){
             System.out.println("division par zero");
         }
 
+    // si b=0 on dit que c'est une division par zéro sans buger le programme
     }
 
     public static void main(String[] args) {
