5cf566a7711d92bd87d7dc9b691b715ec90ba53f
Ammar Kheder <ammar.kheder.auditeur@lecnam.net>
2021-12-08 11:46:12 +0100

    Ex1+Ex2

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Division {
 
   public double division1(int a, int b){
       return (double)a/b;
+  }
+  public double division2(int a, int b) {
+      try {
+          return a / b;
+      } catch (ArithmeticException ex) {
+          System.out.println("Division par zéro");
+      }
+      return 0;
+  }
+
+  public double division3(int a, int b) throws IOException{
+    if (b==0){
+        throw new IOException();
+    }
+    return a/b;
   }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void ex1() {
@@ -8,8 +10,28 @@ public class Main {
         System.out.println(resultat);
 
     }
-    public static void main(String[] args) {
+    public static void ex2(){
+        Division div=new Division();
+        double resultat=div.division2(10,0);
+        System.out.println(resultat);
+    }
 
-	    ex1();
+    public static void ex3() {
+        Division c = new Division();
+
+        try { 
+            double res = c.division3(10, 0);
+            System.out.println(res);
+        }catch (IOException e){
+            System.out.println("division par zero");
+        }
+
+    }
+
+    public static void main(String[] args) {
+    //Ici on appelle l'excercice qu'on veut exécuter
+	   // ex1();
+        //  ex2();
+        ex3();
     }
 }
