a6e59c8163c57cd875ae5eb4324d064c3c448375
Ammar Kheder <ammar.kheder.auditeur@lecnam.net>
2021-12-08 10:42:08 +0100

    Ex1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
 
+    public static void ex1() {
+        Division c = new Division();
+        double resultat = c.division1(5,2);
+        System.out.println(resultat);
+
+    }
     public static void main(String[] args) {
-	// write your code here
+
+	    ex1();
     }
 }
